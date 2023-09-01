54a47d21aec49344f6c3fa15f45fa05e0ea24ad5
David-Koshkadze <rootUS3rr@pm.me>
2023-09-01 12:50:00 +0400

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,22 @@
 export default function Header() {
   return (
-    <header className="bg-green-100 bg-hero-desktop">
-      <nav className="flex justify-between items-center border py-4">
-        <h1>loopstudios</h1>
-        <div className="flex gap-4">
-          <a href="#about">About</a>
-          <a href="#careers">Careers</a>
-          <a href="#events">Events</a>
-          <a href="#products">Products</a>
-          <a href="#support">Support</a>
-        </div>
-      </nav>
+    <header className="h-screen bg-hero-desktop bg-cover bg-no-repeat text-white pb-10">
+      <div className="max-w-[1440px] mx-auto">
+        <nav className="flex justify-between items-center border py-4">
+          <h1 className="font-semibold text-xl">loopstudios</h1>
+          <div className="flex gap-4 text-sm">
+            <a href="#about">About</a>
+            <a href="#careers">Careers</a>
+            <a href="#events">Events</a>
+            <a href="#products">Products</a>
+            <a href="#support">Support</a>
+          </div>
+        </nav>
 
-      <div>
-        <h1>immersive experiences that deliver</h1>
+        <div className="border-2 border-white my-10 p-8 w-[450px]">
+          <h1 className="text-5xl uppercase ">immersive experiences that deliver</h1>
+        </div>
       </div>
     </header>
-  )
+  );
 }
